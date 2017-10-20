d62e5fb9778aaf2c82ccb839eadad70369a2ab11
Gage <ghg13c@my.fsu.edu>
2017-10-20 17:08:08 -0400

    Added an off center button to link to the random wheel page. Also added a random wheel page. Also added a string array for the classes in strings.xml.

---
+++ app/src/main/java/com/example/ivoid/RandomWheelActivity.java
@@ -0,0 +1,15 @@
+package com.example.ivoid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Gage on 10/20/2017.
+ */
+
+public class RandomWheelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.randomwheel);
+    }
+}
