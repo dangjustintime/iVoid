3fb0f84cbc90a8ce12373fe38a547da7633c340d
Justin Dang <dangjustintime@gmail.com>
2017-10-21 10:27:05 -0400

    added name, title, lore, and ingame stats to champion class

--- app/src/main/java/com/example/ivoid/Model/Champion.java
+++ app/src/main/java/com/example/ivoid/Model/Champion.java
@@ -5,7 +5,39 @@ package com.example.ivoid.Model;
  */
 
 public class Champion {
-    private static String name;
+    //name, title, lore
+    private String name;
+    private String title;
+    private String lore;
+    //stats
+    private int difficulty;
+    private int attack;
+    private int defense;
+    private int magic;
+
+    private double armorPerLevel;
+    private double hpPerLevel;
+    private double attackDamage;
+    private double mpPerLevel;
+    private double attackSpeedOffset;
+    private double armor;
+    private double hp;
+    private double hpRegenPerLevel;
+    private double spellBlock;
+    private double attackRange;
+    private double moveSpeed;
+    private double attackDamagePerLevel;
+    private double mpRegenPerLevel;
+    private double mp;
+    private double spellBlockPerLevel;
+    private double crit;
+    private double mpRegen;
+    private double attackSpeedPerLevel;
+    private double hpRegen;
+    private double critPerLevel;
+
+
+
 
 
 }
