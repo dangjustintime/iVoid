0ccf47bbd5cc75f0c6de99d9ffdcc99f5ca75481
Justin Dang <dangjustintime@gmail.com>
2017-10-21 11:47:05 -0400

    added empty Items class and color values for color scheme

--- app/src/main/java/com/example/ivoid/Model/Champion.java
+++ app/src/main/java/com/example/ivoid/Model/Champion.java
@@ -36,8 +36,221 @@ public class Champion {
     private double hpRegen;
     private double critPerLevel;
 
+    //getters and setters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public double getArmorPerLevel() {
+        return armorPerLevel;
+    }
+
+    public void setArmorPerLevel(double armorPerLevel) {
+        this.armorPerLevel = armorPerLevel;
+    }
+
+    public double getHpPerLevel() {
+        return hpPerLevel;
+    }
+
+    public void setHpPerLevel(double hpPerLevel) {
+        this.hpPerLevel = hpPerLevel;
+    }
+
+    public double getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(double attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public double getMpPerLevel() {
+        return mpPerLevel;
+    }
+
+    public void setMpPerLevel(double mpPerLevel) {
+        this.mpPerLevel = mpPerLevel;
+    }
+
+    public double getAttackSpeedOffset() {
+        return attackSpeedOffset;
+    }
+
+    public void setAttackSpeedOffset(double attackSpeedOffset) {
+        this.attackSpeedOffset = attackSpeedOffset;
+    }
+
+    public double getArmor() {
+        return armor;
+    }
+
+    public void setArmor(double armor) {
+        this.armor = armor;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+
+    public double getHpRegenPerLevel() {
+        return hpRegenPerLevel;
+    }
+
+    public void setHpRegenPerLevel(double hpRegenPerLevel) {
+        this.hpRegenPerLevel = hpRegenPerLevel;
+    }
+
+    public double getSpellBlock() {
+        return spellBlock;
+    }
+
+    public void setSpellBlock(double spellBlock) {
+        this.spellBlock = spellBlock;
+    }
+
+    public double getAttackRange() {
+        return attackRange;
+    }
+
+    public void setAttackRange(double attackRange) {
+        this.attackRange = attackRange;
+    }
+
+    public double getMoveSpeed() {
+        return moveSpeed;
+    }
+
+    public void setMoveSpeed(double moveSpeed) {
+        this.moveSpeed = moveSpeed;
+    }
+
+    public double getAttackDamagePerLevel() {
+        return attackDamagePerLevel;
+    }
+
+    public void setAttackDamagePerLevel(double attackDamagePerLevel) {
+        this.attackDamagePerLevel = attackDamagePerLevel;
+    }
+
+    public double getMpRegenPerLevel() {
+        return mpRegenPerLevel;
+    }
+
+    public void setMpRegenPerLevel(double mpRegenPerLevel) {
+        this.mpRegenPerLevel = mpRegenPerLevel;
+    }
+
+    public double getMp() {
+        return mp;
+    }
+
+    public void setMp(double mp) {
+        this.mp = mp;
+    }
+
+    public double getSpellBlockPerLevel() {
+        return spellBlockPerLevel;
+    }
+
+    public void setSpellBlockPerLevel(double spellBlockPerLevel) {
+        this.spellBlockPerLevel = spellBlockPerLevel;
+    }
+
+    public double getCrit() {
+        return crit;
+    }
+
+    public void setCrit(double crit) {
+        this.crit = crit;
+    }
+
+    public double getMpRegen() {
+        return mpRegen;
+    }
+
+    public void setMpRegen(double mpRegen) {
+        this.mpRegen = mpRegen;
+    }
+
+    public double getAttackSpeedPerLevel() {
+        return attackSpeedPerLevel;
+    }
+
+    public void setAttackSpeedPerLevel(double attackSpeedPerLevel) {
+        this.attackSpeedPerLevel = attackSpeedPerLevel;
+    }
+
+    public double getHpRegen() {
+        return hpRegen;
+    }
+
+    public void setHpRegen(double hpRegen) {
+        this.hpRegen = hpRegen;
+    }
+
+    public double getCritPerLevel() {
+        return critPerLevel;
+    }
+
+    public void setCritPerLevel(double critPerLevel) {
+        this.critPerLevel = critPerLevel;
+    }
 
 }
---
+++ app/src/main/java/com/example/ivoid/Model/Item.java
@@ -0,0 +1,9 @@
+package com.example.ivoid.Model;
+
+/**
+ * Created by Justin Dang on 10/21/2017.
+ */
+
+public class Item {
+
+}
