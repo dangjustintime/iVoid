823687366818125a27e100d3023be6437e10a667
Justin Dang <dangjustintime@gmail.com>
2017-10-25 10:28:00 -0400

    added offical api url and api key to strings.xml

--- app/src/main/java/com/example/ivoid/MainActivity.java
+++ app/src/main/java/com/example/ivoid/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.ivoid;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
