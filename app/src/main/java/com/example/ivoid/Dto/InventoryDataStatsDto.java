466780d513887ff79c3f3d6dd38f8c1563c08755
Bo Cameron <bogarrettcameron@gmail.com>
2017-11-03 14:09:02 -0400

    Added Dto classes for items. Added a function to the main activity to parse a local json file. Also the text has been updated to be white for the menu bar.

---
+++ app/src/main/java/com/example/ivoid/Dto/InventoryDataStatsDto.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ivoid.Dto;
+
+/**
+ *
+ * @author BoCameron
+ */
+public class InventoryDataStatsDto {
+	double PercentCritDamageMod;
+	double PercentSpellBlockMod;
+	double PercentHPRegenMod;
+	double PercentMovementSpeedMod;
+	double FlatSpellBlockMod;
+	double FlatCritDamageMod;
+	double FlatEnergyPoolMod;
+	double PercentLifeStealMod;
+	double FlatMPPoolMod;
+	double FlatMovementSpeedMod;
+	double PercentAttackSpeedMod;
+	double FlatBlockMod;
+	double PercentBlockMod;
+	double FlatEnergyRegenMod;
+	double PercentSpellVampMod;
+	double FlatMPRegenMod;
+	double PercentDodgeMod;
+	double FlatAttackSpeedMod;
+	double FlatArmorMod;
+	double FlatHPRegenMod;
+	double PercentMagicDamageMod;
+	double PercentMPPoolMod;
+	double FlatMagicDamageMod;
+	double PercentMPRegenMod;
+	double PercentPhysicalDamageMod;
+	double FlatPhysicalDamageMod;
+	double PercentHPPoolMod;
+	double PercentArmorMod;
+	double PercentCritChanceMod;
+	double PercentEXPBonus;
+	double FlatHPPoolMod;
+	double FlatCritChanceMod;
+	double FlatEXPBonus;
+}
