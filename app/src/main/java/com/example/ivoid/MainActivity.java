8a1138a185c832c28fe324d6a52e5dece02796bf
Justin Dang <dangjustintime@gmail.com>
2017-11-14 22:19:13 -0500

    created activities for all the sections and set onClicks to all of them from main activity

--- app/src/main/java/com/example/ivoid/MainActivity.java
+++ app/src/main/java/com/example/ivoid/MainActivity.java
@@ -3,24 +3,59 @@ package com.example.ivoid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.ivoid.Dto.ItemListDto;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.example.ivoid.Dto.*;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //views
+    public ImageButton itemsImageButton;
+    public ImageButton championsImageButton;
+    public ImageButton updatesImageButton;
+    public ImageButton randomImageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //bind views to layout views
+        itemsImageButton = (ImageButton) findViewById(R.id.image_button_items_main);
+        championsImageButton = (ImageButton) findViewById(R.id.image_button_champions_main);
+        updatesImageButton = (ImageButton) findViewById(R.id.image_button_updates_main);
+        randomImageButton = (ImageButton) findViewById(R.id.image_button_random_main);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+
+    public void itemsClick(View v) {
+        //Start ItemsActivity
+        Intent intent = new Intent (MainActivity.this, ItemsActivity.class);
+        startActivity(intent);
+    }
+
+    public void championsClick(View v) {
+        //Start ItemsActivity
+        Intent intent = new Intent (MainActivity.this, ChampionsActivity.class);
+        startActivity(intent);
+    }
+
+    public void updatesClick(View v) {
+        //Start ItemsActivity
+        Intent intent = new Intent (MainActivity.this, UpdatesActivity.class);
+        startActivity(intent);
+    }
+
     public void randomClick(View v) {
         //Start RandomWheelActivity
         Intent intent = new Intent (MainActivity.this, RandomWheelActivity.class);
