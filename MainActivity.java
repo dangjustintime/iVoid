ad4f717cd9405f965c3578118b0c3d11847e1a00
aylex45 <32654634+aylex45@users.noreply.github.com>
2017-11-20 23:55:17 -0500

    Add files via upload
    MainActivity : Update imagebutton and random button removed. 
    string.xml : update string and latest feat string added

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.ivoid;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.ivoid.Dto.ItemListDto;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,19 +24,24 @@ import java.io.Reader;
 public class MainActivity extends AppCompatActivity {
 
     //views
+    public ImageView velKozImageView;
     public ImageButton itemsImageButton;
     public ImageButton championsImageButton;
     public ImageButton updatesImageButton;
     public ImageButton randomImageButton;
+    public Button patchNotesClick;
+    public Button latestNewsClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //bind views to layout views
+        velKozImageView = (ImageView) findViewById(R.id.vel_koz_image_view);
         itemsImageButton = (ImageButton) findViewById(R.id.image_button_items_main);
         championsImageButton = (ImageButton) findViewById(R.id.image_button_champions_main);
         updatesImageButton = (ImageButton) findViewById(R.id.image_button_updates_main);
         randomImageButton = (ImageButton) findViewById(R.id.image_button_random_main);
-
+        patchNotesClick = (Button) findViewById(R.id.patch_notes_button);
+        latestNewsClick = (Button) findViewById(R.id.latest_news_button);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -51,31 +60,35 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void updatesClick(View v) {
-        //Start ItemsActivity
-        Intent intent = new Intent (MainActivity.this, UpdatesActivity.class);
-        startActivity(intent);
-    }
 
     public void randomClick(View v) {
         //Start RandomWheelActivity
         Intent intent = new Intent (MainActivity.this, RandomWheelActivity.class);
         startActivity(intent);
     }
-    public void updateData(View v) throws IOException {
-        try(Reader reader = new InputStreamReader(MainActivity.class.getResourceAsStream("assets/items.json"), "UTF-8")){
-            Gson gson = new GsonBuilder().create();
-            ItemListDto itemList = gson.fromJson(reader, ItemListDto.class);
-            Log.v("ITEM LIST: ",itemList.version);
 
-        }
+
+
+    public void patchNotesClick(View v){
+
+        String url = "https://na.leagueoflegends.com/en/news/game-updates/patch/patch-!-notes";
+
+        String apicall = "723";
+        String replaceString = url.replaceAll("!",apicall );
+
+        if (!replaceString.startsWith("http://") && !replaceString.startsWith("https://"))
+            replaceString = "http://" + replaceString;
+
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(replaceString));
+        startActivity(i);
     }
-    public void updatePage(View v){
 
-        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://na.leagueoflegends.com/en/news/game-updates/patch/patch-722-notes"));
-        //    startActivity(intent);
+    public void latestNewsClick(View v){
 
-        String url = "https://na.leagueoflegends.com/en/news/game-updates/patch/patch-722-notes";
+
+        String url = "https://na.leagueoflegends.com/en/news/";
 
         if (!url.startsWith("http://") && !url.startsWith("https://"))
             url = "http://" + url;
@@ -83,10 +96,6 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
-
-
-
     }
-
 
 }
