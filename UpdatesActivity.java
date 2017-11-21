7ce57e98e5e53ed65afe5f9cb868e5331b3baf2f
aylex45 <32654634+aylex45@users.noreply.github.com>
2017-11-20 22:43:32 -0500

    Add files via upload
    SingleTop launchmode added to androidmanifest which only allows one instance of an activity.
    
    URL substring manipulator added to updates activity and version 723 was hardcoded in.

--- UpdatesActivity.java
+++ UpdatesActivity.java
@@ -17,7 +17,33 @@ public class UpdatesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_updates);
     }
 
+    public void patchNotesClick(View v){
+
+        String url = "https://na.leagueoflegends.com/en/news/game-updates/patch/patch-!-notes";
+
+        String apicall = "723";
+        String replaceString = url.replaceAll("!",apicall );
+
+        if (!replaceString.startsWith("http://") && !replaceString.startsWith("https://"))
+            replaceString = "http://" + replaceString;
 
 
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(replaceString));
+        startActivity(i);
+    }
+    public void latestNewsClick(View v){
 
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://na.leagueoflegends.com/en/news/game-updates/patch/patch-722-notes"));
+        //    startActivity(intent);
+
+        String url = "https://na.leagueoflegends.com/en/news/";
+
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
 }
